cece216728fa8bd86a63e3b4353b00019efb5876
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-05-17 20:27:27 +0100

    Update DateTime.java

--- DateTime.java
+++ DateTime.java
@@ -1,9 +1,17 @@
-import java.time.DateTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
   
   public static void main(String[] args) {
+    
+    LocalDateTime dateTimeObj = LocalDateTime.now();
+    
+    DateTimeFormatter formattedObj = DateTimeFormatter.ofPattern("dd-MM-yyy HH:mm:ss:ns");
+    
+    String finalFormat = dateTimeObj.format(formattedObj);
+    
+    
   
   
   
